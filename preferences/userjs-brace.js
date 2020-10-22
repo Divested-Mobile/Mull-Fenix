be2f14d0a888090357bdda7e77cfdeaea6e31b01
Tad <tad@spotco.us>
2020-10-22 06:25:23 -0400

    Make it Mull
    Signed-off-by: Tad <tad@spotco.us>

---
+++ preferences/userjs-brace.js
@@ -0,0 +1,33 @@
+//Look
+pref("browser.ctrlTab.recentlyUsedOrder", false);
+pref("browser.privatebrowsing.vpnpromourl", "");
+pref("browser.tabs.drawInTitlebar", true);
+pref("devtools.netmonitor.persistlog", true);
+pref("devtools.webconsole.persistlog", true);
+pref("dom.vibrator.enabled", false);
+pref("general.smoothScroll", false);
+pref("widget.allow-client-side-decoration", true);
+pref("mailnews.start_page.enabled", false);
+
+//Privacy
+pref("browser.contentblocking.ui.enabled", true);
+pref("browser.link.open_newwindow_restriction", true);
+pref("browser.snippets.enabled", false);
+pref("browser.snippets.firstrunHomepage.enabled", false);
+pref("browser.snippets.syncPromo.enabled", false);
+pref("browser.snippets.updateUrl", "");
+pref("dom.battery.enabled", false);
+pref("general.useragent.updates.enabled", false);
+pref("network.negotiate-auth.trusted-uris", "");
+pref("network.trr.custom_uri", "https://dns.quad9.net/dns-query");
+pref("plugin.expose_full_path", false);
+pref("extensions.enigmail.autoWkdLookup", 0);
+pref("messenger.status.reportIdle", false);
+
+//Security
+pref("browser.gnome-search-provider.enabled", false);
+pref("browser.preferences.exposeHTTPSOnly", true);
+//pref("dom.security.https_only_mode", true); //MULL-COMMENTED
+pref("security.webauth.u2f", true);
+pref("mail.phishing.detection.enabled", true);
+pref("mailnews.message_display.disable_remote_image", true);
