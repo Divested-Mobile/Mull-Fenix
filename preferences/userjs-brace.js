7918360cbcf64bebab4a5a17780873820aed255c
Tad <tad@spotco.us>
2021-08-26 16:31:10 -0400

    pull in preferences from brace
    this disables the jit like brace
    might need to be disabled if it is too slow on mobile
    
    Signed-off-by: Tad <tad@spotco.us>

--- preferences/userjs-brace.js
+++ preferences/userjs-brace.js
@@ -4,28 +4,25 @@ pref("browser.privatebrowsing.vpnpromourl", "");
 pref("browser.tabs.drawInTitlebar", true);
 pref("devtools.netmonitor.persistlog", true);
 pref("devtools.webconsole.persistlog", true);
+pref("dom.vibrator.enabled", false);
 pref("general.smoothScroll", false);
 pref("widget.allow-client-side-decoration", true);
 pref("mailnews.start_page.enabled", false);
 
 //Privacy
+pref("browser.contentblocking.ui.enabled", true);
 pref("browser.link.open_newwindow_restriction", true);
 pref("browser.snippets.enabled", false);
 pref("browser.snippets.firstrunHomepage.enabled", false);
 pref("browser.snippets.syncPromo.enabled", false);
 pref("browser.snippets.updateUrl", "");
+pref("dom.battery.enabled", false);
 pref("general.useragent.updates.enabled", false);
 pref("network.negotiate-auth.trusted-uris", "");
 pref("network.trr.custom_uri", "https://dns.quad9.net/dns-query");
 pref("plugin.expose_full_path", false);
 pref("extensions.enigmail.autoWkdLookup", 0);
 pref("messenger.status.reportIdle", false);
-
-//Tracking Protection
-pref("browser.contentblocking.ui.enabled", true);
-pref("privacy.trackingprotection.enabled", true);
-pref("privacy.trackingprotection.cryptomining.enabled", true);
-pref("privacy.trackingprotection.fingerprinting.enabled", true);
 
 //Security
 pref("browser.gnome-search-provider.enabled", false);
