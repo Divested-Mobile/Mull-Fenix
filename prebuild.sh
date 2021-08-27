287498d5b3350f8c06e674c760848ba8d9cc63fd
Tad <tad@spotco.us>
2021-08-27 10:42:08 -0400

    Always show the Quit button
    Signed-off-by: Tad <tad@spotco.us>

--- prebuild.sh
+++ prebuild.sh
@@ -111,6 +111,12 @@ sed -i \
     -e 's/!defaultBrowserNotificationDisplayed && !isDefaultBrowserBlocking()/false/' \
     app/src/main/java/org/mozilla/fenix/utils/Settings.kt
 
+# Always show the Quit button
+sed -i \
+    -e 's/if (settings.shouldDeleteBrowsingDataOnQuit) quitItem else null/quitItem/' \
+    -e '/val settings = context.components.settings/d' \
+    app/src/main/java/org/mozilla/fenix/home/HomeMenu.kt
+
 # Set up target parameters
 case $(echo "$2" | cut -c 5) in
     0)
