dbe3d919a8863760f1efcac02829f98b540e48ef
Tad <tad@spotco.us>
2021-08-26 21:33:56 -0400

    do not disable the jit
    way too slow on mobile compared to desktop
    
    Signed-off-by: Tad <tad@spotco.us>

--- preferences/userjs-arkenfox.js
+++ preferences/userjs-arkenfox.js
@@ -1204,9 +1204,9 @@ pref("javascript.options.asmjs", false); //BRACE-UNCOMMENTED
  * [NOTE] In FF75+, when **both** Ion and JIT are disabled, **and** the new
  * hidden pref is enabled, then Ion can still be used by extensions (1599226)
  * [1] https://cve.mitre.org/cgi-bin/cvekey.cgi?keyword=firefox+jit ***/
-pref("javascript.options.ion", false); //BRACE-UNCOMMENTED
-pref("javascript.options.baselinejit", false); //BRACE-UNCOMMENTED
-pref("javascript.options.jit_trustedprincipals", true); // [FF75+] [HIDDEN PREF] //BRACE-UNCOMMENTED
+   // pref("javascript.options.ion", false); //BRACE-UNCOMMENTED //MULL-COMMENTED
+   // pref("javascript.options.baselinejit", false); //BRACE-UNCOMMENTED //MULL-COMMENTED
+   // pref("javascript.options.jit_trustedprincipals", true); // [FF75+] [HIDDEN PREF] //BRACE-UNCOMMENTED //MULL-COMMENTED
 /* 5506: disable WebAssembly [FF52+]
  * Vulnerabilities [1] have increasingly been found, including those known and fixed
  * in native programs years ago [2]. WASM has powerful low-level access, making
