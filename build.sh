b9f276866988477408db8b3fed657ce074e46168
Tavi <tavi@divested.dev>
2024-11-04 13:45:21 -0500

    Mull 132.0.1
    Signed-off-by: Tavi <tavi@divested.dev>

--- build.sh
+++ build.sh
@@ -46,10 +46,17 @@ cargo install --force --vers 0.26.0 cbindgen
 pushd $llvm
 llvmtarget=$(cat "$llvm/targets_to_build")
 echo "building llvm for $llvmtarget"
+if grep -q "Fedora" /etc/os-release; then
+cmake -S llvm -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=out -DCMAKE_C_COMPILER=clang-18 \
+    -DCMAKE_CXX_COMPILER=clang++-18 -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_TARGETS_TO_BUILD="$llvmtarget" \
+    -DLLVM_USE_LINKER=lld -DLLVM_BINUTILS_INCDIR=/usr/include -DLLVM_ENABLE_PLUGINS=FORCE_ON \
+    -DLLVM_DEFAULT_TARGET_TRIPLE="x86_64-unknown-linux-gnu"
+else
 cmake -S llvm -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=out -DCMAKE_C_COMPILER=clang-16 \
     -DCMAKE_CXX_COMPILER=clang++-16 -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_TARGETS_TO_BUILD="$llvmtarget" \
     -DLLVM_USE_LINKER=lld -DLLVM_BINUTILS_INCDIR=/usr/include -DLLVM_ENABLE_PLUGINS=FORCE_ON \
     -DLLVM_DEFAULT_TARGET_TRIPLE="x86_64-unknown-linux-gnu"
+fi;
 cmake --build build -j"$(nproc)"
 cmake --build build --target install -j"$(nproc)"
 popd
